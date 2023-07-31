1cf2cc9e5ab439a69e7cd1b2041f4193c055b647
prakash.jhugroo <prakash.jhugroo@agileum.com>
2023-07-31 20:36:03 +0400

    #main

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -68,7 +68,7 @@ function InfiniteTasksList({ todos, isError, isLoading, fetchNewTasks, hasMore =
     if (isLoading) return <LoadingSpinner />;
     if (isError) return <h1>Error...</h1>;
     if (todos == null) return null;
-    if (todos == null || todos.length === 0) return <h2 className="my-4 text-center text-2x; text-gray-500">No tweets</h2>;
+    if (todos == null || todos.length === 0) return <h2 className="my-4 text-center text-2x; text-gray-500">No tasks</h2>;
 
     return <ul>
         <InfiniteScroll
